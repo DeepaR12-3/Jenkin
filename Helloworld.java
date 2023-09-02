4ffc3737f1c15bb4bce390b8674e892751017b75
Deepanaik <deepashreenaik1057@gmail.com>
2023-09-02 09:54:42 +0530

    changed

--- Helloworld.java
+++ Helloworld.java
@@ -2,5 +2,6 @@ class Helloworld {
      public static void main (String args[]) {
          System.out.println("Welcom to java program");
          System.out.println("JAva");
+         System.out.println("Program");
  }
 }
